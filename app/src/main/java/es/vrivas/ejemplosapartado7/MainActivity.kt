d0b7f80d2dc291bc9b3b55bf02ecdc87181e1c23
Victor Rivas <vrivas@ujaen.es>
2020-10-30 17:56:05 +0100

    Funcionando toast y snackbar

--- app/src/main/java/es/vrivas/ejemplosapartado7/MainActivity.kt
+++ app/src/main/java/es/vrivas/ejemplosapartado7/MainActivity.kt
@@ -5,10 +5,12 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     val LOG_TAG = "LOG EN MAINACTIVITY"
+    @Suppress("UNUSED_EXPRESSION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,10 +19,26 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Soy un Toast de duración corta", Toast.LENGTH_SHORT ).show()
             true
         }
-        layout.setOnLongClickListener { view->
+        /*layout.setOnLongClickListener { view->
             Toast.makeText(this, "Soy un Toast de duración LARGA!", Toast.LENGTH_LONG ).show()
             true
+        }*/
+
+        // Sustituimos el Toast largo por un snackbar
+        layout.setOnLongClickListener { view->
+            Snackbar.make(
+                view,
+                "Soy un SNACKBAR de duración LARGA!",
+                Snackbar.LENGTH_LONG
+            )
+                .setAction("Pulsa el snackbar"
+                ) {
+                    Log.d( LOG_TAG, "SNACKBAR PULSADO")
+                }
+                .show()
+            true
         }
+
     }
 
 
