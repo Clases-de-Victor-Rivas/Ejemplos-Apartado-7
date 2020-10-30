e793717eff05355bb0b5d82d1c73f70db0faec75
Victor Rivas <vrivas@ujaen.es>
2020-10-30 18:46:11 +0100

    IMplementado alert dialog

--- app/src/main/java/es/vrivas/ejemplosapartado7/MainActivity.kt
+++ app/src/main/java/es/vrivas/ejemplosapartado7/MainActivity.kt
@@ -1,10 +1,12 @@
 package es.vrivas.ejemplosapartado7
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,23 +16,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Usp de Log.d
         Log.d( LOG_TAG, "Ejecutando método onCreate")
+
+        // Uso de TOAST corto
         layout.setOnClickListener {view->
             Toast.makeText(this, "Soy un Toast de duración corta", Toast.LENGTH_SHORT ).show()
             true
         }
+
+        // USO DE TOAST largo
+        // Sustituimos el Toast largo por un snackbar
         /*layout.setOnLongClickListener { view->
             Toast.makeText(this, "Soy un Toast de duración LARGA!", Toast.LENGTH_LONG ).show()
             true
         }*/
 
-        // Sustituimos el Toast largo por un snackbar
+        // Uso de Snackbar con action + Uso de Log.d en dicho action
         layout.setOnLongClickListener { view->
-            Snackbar.make(
-                view,
-                "Soy un SNACKBAR de duración LARGA!",
-                Snackbar.LENGTH_LONG
-            )
+            Snackbar.make( view,"Soy un SNACKBAR de duración LARGA!", Snackbar.LENGTH_LONG )
                 .setAction("Pulsa el snackbar"
                 ) {
                     Log.d( LOG_TAG, "SNACKBAR PULSADO")
@@ -41,9 +46,28 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    // Uso de Alert Dialog
+    @Suppress("UNUSED_PARAMETER")
+    fun boton_dialog_alert_click(View: View) {
+        val dialogo = AlertDialog.Builder(this)
+            .setTitle("Soy el título del diálogo")
+            .setMessage("Soy el mensaje del diálogo")
+            .setPositiveButton("Aceptar"){ dialogInterface: DialogInterface, i: Int ->
+               Log.d( LOG_TAG, "Aceptar diálogo")
+                //dialogInterface.dismiss() // Elimina el diálogo
+            }
+            .setNegativeButton("Cancelar"){ dialogInterface: DialogInterface, i: Int ->
+                Log.d( LOG_TAG, "Cancelar diálogo")
+                //dialogInterface.cancel() // Cancel lanza el evento OnCancel y posteriormente hace dismiss
+            }
+            dialogo.show()
+    }
+
 
     override fun onResume() {
         super.onResume()
+
+        // Uso de Log.e
         Log.e(LOG_TAG, "Ejecutando método onResume")
     }
 
